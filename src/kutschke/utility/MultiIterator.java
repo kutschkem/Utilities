ec686cb65e0a48786a1d09f63b9a83d9c12a01ab
Michael Kutschke <kutschke.michael@googlemail.com>
2011-01-08 21:54:21 +0100

    added Multi-Collection Iterator, removed SideEffect annotation

---
+++ src/kutschke/utility/MultiIterator.java
@@ -0,0 +1,45 @@
+package kutschke.utility;
+
+import java.util.Iterator;
+
+public class MultiIterator<T> implements Iterator<T> {
+	
+	Iterator<? extends Iterable<? extends T>> superIterator;
+	Iterator<? extends T> currentIterator;
+	
+	public MultiIterator(Iterable<? extends Iterable<? extends T>> collection){
+		superIterator = collection.iterator();
+		currentIterator = nextIterator();
+	}
+	
+	private Iterator<? extends T> nextIterator(){
+		while(superIterator.hasNext()){
+			currentIterator = superIterator.next().iterator();
+			if(currentIterator.hasNext()) return currentIterator;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if(currentIterator == null)
+		return false;
+		if(currentIterator.hasNext()) return true;
+		currentIterator = nextIterator();
+		return hasNext();
+	}
+
+	@Override
+	public T next() {
+		if(! hasNext())
+		return null;
+		return currentIterator.next();
+	}
+
+	@Override
+	public void remove() {
+		currentIterator.remove();
+
+	}
+
+}
